5478a1935bfd0604ddcf7bafcfda8c60f5970523
nicolasH <nicolas.hornuel@gmail.com>
2021-02-24 17:30:24 +0100

    tp-jpa-05 SINGLE_TABLE

--- src/banque/TestJpa.java
+++ src/banque/TestJpa.java
@@ -1,8 +1,6 @@
 package banque;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,6 +11,7 @@ import banque.entite.Adresse;
 import banque.entite.Banque;
 import banque.entite.Client;
 import banque.entite.Compte;
+import banque.entite.LivretA;
 import banque.entite.Operation;
 
 public class TestJpa {
@@ -26,42 +25,47 @@ public class TestJpa {
 		et.begin();
 		
 		// 1 BANQUE
+		
 		Banque b1 = new Banque();
 		b1.setNom("La Banque Postale");
+		em.persist(b1);
 		
-		// 2 ADRESSE CLIENT
+		// 2 ADRESSE et CLIENT
+		
 		Adresse a1 = new Adresse();
 		a1.setCodePostale(21000);
 		a1.setNumero(1);
 		a1.setRue("Place de l'Étoile");
 		a1.setVille("DIJON");
+		Client client1 = new Client();
+		client1.setNom("HUBERT");
+		client1.setPrenom("Djo");
+		client1.setDateNaissance(new Date());
+		client1.setAdresse(a1);
+		em.persist(client1);
 		
-		// 3 CLIENT
-		Client cl1 = new Client();
-		cl1.setNom("HUBERT");
-		cl1.setPrenom("Djo");
-		cl1.setDateNaissance(new Date());
-		cl1.setAdresse(a1);
+		// 3 COMPTE
 		
-		// 4 COMPTE
-		Set<Client> clients = new HashSet<>();
-		clients.add(cl1);
 		Compte ct1 = new Compte();
-		ct1.setClients(clients);
 		ct1.setNumero("463786287-A");
-		ct1.setOperations(null);
 		ct1.setSolde(200);
+		em.persist(ct1);
+		client1.getComptes().add(ct1);
 				
+		LivretA lva1 = new LivretA();
+		lva1.setNumero("89798986-HG");
+		lva1.setSolde(300);
+		lva1.setTaux(1.4);
+		em.persist(lva1);
+		client1.getComptes().add(lva1);
+		
 		// 5 OPERATION
+		
 		Operation op1 = new Operation();
 		op1.setCompte(ct1);
 		op1.setDate(new Date());
 		op1.setMontant(150);
 		op1.setMotif("Retrait Guichet");
-			
-		em.persist(b1);	
-		em.persist(cl1);	
-		em.persist(ct1);
 		em.persist(op1);
 		
 		et.commit();
